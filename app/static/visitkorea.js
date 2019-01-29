6ae93df9f82f3db03388cf507794221aa180700b
Ioseph Kim <iosephkim@gmail.com>
2019-01-29 16:38:35 +0900

    sample done

---
+++ app/static/visitkorea.js
@@ -0,0 +1,172 @@
+function click_navi(){
+	var locaval = "";
+	var cateval = "";
+	var check_subloca = 0;
+	$("#sublocanavi input:checkbox").each(function() {
+		check_subloca += 1;
+	       if($(this).is(":checked")){
+		       if(locaval != "") locaval += ",";
+		       locaval += $(this).val(); 
+	       }
+	});
+	if(check_subloca == 0) locaval = "undefind";
+	$("#catenavi input:checkbox").each(function() {
+	       if($(this).is(":checked")){
+		       if(cateval != "") cateval += ",";
+		       cateval += $(this).val(); 
+	       }
+	});
+	locaval = (locaval == "") ? "all" : locaval;
+	cateval = (cateval == "") ? "all" : cateval;
+
+	$.ajax({
+		url:'/ajax/list/' + locaval + "/" + cateval,
+		success:function(data){
+			$("#placelist").empty();
+			$.each(data, function(num, row){
+				if(! row.imgurl) imgurl = "http://api.visitkorea.or.kr/static/images/common/noImage.gif";
+				else imgurl = row.imgurl;
+				$("#placelist").append("<li onclick='self.location.href=\"/place/" + row.place_id + "\"'>" + row.place_name + "<br />" + "<img src=" + imgurl + " height='128' />"+ "</li>");
+			});
+		}
+	});
+}
+
+function display_location(){
+	$.ajax({
+		url:'/ajax/location/',
+		success:function(data){
+			$.each(data, function(num, row){
+				$("#locanavi").append("<li><input type='radio' name='toploca' value='" + row.addrid + "' />" + row.addrname + "</li>");
+			});
+			$("#locanavi").append("<li><input type='radio' name='toploca' value='undefind' />미지정</li>");
+
+			$("#locanavi input:radio").change(function(obj){ display_subnavi(this.value); });
+
+		}
+	});
+}
+
+
+function display_subnavi(toploca){
+	if(toploca != "undefind"){
+	$.ajax({
+		url:'/ajax/location/' + toploca,
+		success:function(data){
+			$("#sublocanavi").empty();
+			$.each(data, function(num, row){
+				$("#sublocanavi").append("<li><input type='checkbox' name='subloca' value='" + row.addrid + "' />" + row.addrname + "</li>");
+			});
+
+			$("#sublocanavi input:checkbox").change(function(obj){ click_navi() });
+
+		}
+
+	});
+	}
+	else {
+			$("#sublocanavi").empty();
+			click_navi();
+	}
+}
+
+function display_category(){
+	$.ajax({
+		url:'/ajax/category/',
+		success:function(data){
+			$.each(data, function(num, row){
+				$("#catenavi").append("<li><input type='checkbox' value='" + row.tourid + "' />" + row.tourname + "</li>");
+			});
+			$("#catenavi input:checkbox").change(function(obj){ click_navi(); });
+
+		}
+	});
+}
+
+function display_randomlist(){
+	$.ajax({
+		url:'/ajax/random10/',
+		success:function(data){
+			$("#placelist").empty();
+			$.each(data, function(num, row){
+				if(! row.imgurl) imgurl = "http://api.visitkorea.or.kr/static/images/common/noImage.gif";
+				else imgurl = row.imgurl;
+				$("#placelist").append("<li onclick='self.location.href=\"/place/" + row.place_id + "\"'>" + row.place_name + "<br />" + "<img src=" + imgurl + " height='128' />"+ "</li>");
+			});
+
+		}
+	});
+}
+
+function startpage(){
+	display_location();
+	display_category();
+	display_randomlist();
+	$("#input_search").keypress(function(e) {
+		if (e.keyCode == 13){
+
+	$.ajax({
+		url:'/ajax/namesearch/' + $("#input_search").val(),
+		success:function(data){
+			$("#placelist").empty();
+			$.each(data, function(num, row){
+				if(! row.imgurl) imgurl = "http://api.visitkorea.or.kr/static/images/common/noImage.gif";
+				else imgurl = row.imgurl;
+				$("#placelist").append("<li onclick='self.location.href=\"/place/" + row.place_id + "\"'>" + row.place_name + "<br />" + "<img src=" + imgurl + " height='128' />"+ "</li>");
+			});
+
+		}
+	});
+
+		}    
+	});
+}
+
+function startplace(place_id, x, y){
+	$.ajax({
+		url:'/ajax/getnames/' +  $("#placelocation").text() + '/' + $("#placecategory").text(),
+		success:function(data){
+			$("#placelocation").text(data.locaname);
+			$("#placecategory").text(data.catename);
+		}
+	});
+	$.ajax({
+		url:'/ajax/getattrib/' +  place_id,
+		success:function(data){
+			$.each(data, function(num, row){
+				$("#placedl").append("<dt>" + row.attname + "</dt>");
+				$("#placedl").append("<dd>" + row.v + "</dd>");
+			});
+		}
+
+	});
+
+
+	$.ajax({
+		url:'/ajax/getimages/' +  place_id,
+		success:function(data){
+			$.each(data, function(num, row){
+				$("#place_images").append("<div><img src='" + row.imgurl + "' height='400' /></div>");
+			});
+
+			$('#place_images').bbslider({
+				controls:   true,
+				transition: 'slide',
+				easing:     'easeInOutExpo',
+				duration:   500
+			});
+		}
+	});
+
+	$.ajax({
+		url:'/ajax/near/' + place_id + '/' +  x + '/' + y,
+		success:function(data){
+			$.each(data, function(num, row){
+				if(! row.imgurl) imgurl = "http://api.visitkorea.or.kr/static/images/common/noImage.gif";
+				else imgurl = row.imgurl;
+				$("#nearlist").append("<div onclick='self.location.href=\"/place/" + row.place_id + "\"'>[" + row.tourname + '] ' + row.place_name + "(" + row.distance + "km)<br />" + "<img src=" + imgurl + " height='128' />"+ "</div>");
+
+			});
+		}
+	});
+}
--- app/visitkorea.py
+++ app/visitkorea.py
@@ -1,44 +1,158 @@
+# -*- coding: utf-8 -*-
 import os
 from flask import Flask
 from flask import render_template
 from flask import request
+from flask import Response
+from flask import g
+from flask import jsonify, json
 from flask import send_from_directory
 import psycopg2
 import psycopg2.extras
 from urllib.parse import quote_plus
+from flask_sqlalchemy import SQLAlchemy
 from pprint import pprint
+
 
 app = Flask(__name__)
 
+app.config['SECRET_KEY'] = '9b1780f7bce9960bfefc34f0d52f0690'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost:5433/ioseph'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+def sql(rawSql, sqlVars={}):
+    "Execute raw sql, optionally with prepared query"
+    assert type(rawSql)==str
+    assert type(sqlVars)==dict
+    res=db.session.execute(rawSql, sqlVars)
+    db.session.commit()
+    return res
+
 
 @app.route('/', methods=['POST', 'GET'])
-def hello_world():
-    f = request.args.get('f', 's').strip()
-    v = request.args.get('v', '<http://data.visitkorea.or.kr/>').strip()
-
-    d = {}    
-    d['v'] = v
-    d['fs'] = ('s', 'p', 'ot', 'ov')
-    
-    if f not in d['fs']:
-        f = 's'
-    d['f'] = f
-
-    conn = psycopg2.connect('user=postgres port=5432 host=/tmp')
-    cur = conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
-    query = "select * from visitkorea where {} = %s".format(f)
-    cur.execute(query, (v,))
-
-    d['rows'] = cur.fetchall();
-    d['quote_plus'] = quote_plus
-    cur.close()
-    conn.close()
-    
+def start_page():
+    d = {"title": "PostgreSQL + Flask + jquery 로 구축하는 대한민국 구석구석"}
     return render_template('index.html', d = d)
+
+@app.route('/place/<int:place_id>')
+def view_resource(place_id):
+    query = "select * from place where place_id = :place_id"
+    d = sql(query, {'place_id': place_id})
+    return render_template('place.html', d = d.fetchone())
 
 @app.route('/favicon.ico')
 def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
+@app.route('/ajax/getattrib/<int:place_id>')
+def get_attribute(place_id):
+    query = "select b.attname, a.v from place_attrib a, attnames b where a.place_id = :place_id and a.attid = b.attid order by b.sortnum"
+    d = sql(query, {'place_id': place_id})
+    return jsonify([dict(row) for row in d.fetchall()])
+
+@app.route('/ajax/getimages/<int:place_id>')
+def get_images(place_id):
+    query = "select imgurl from place_images where place_id = :place_id order by 1"
+    d = sql(query, {'place_id': place_id})
+    return jsonify([dict(row) for row in d.fetchall()])
+
+
+@app.route('/ajax/location')
+@app.route('/ajax/location/')
+@app.route('/ajax/location/<upaddr>')
+def location_list(upaddr = ''):
+    query = "select addrid,addrname from addrcodes where upaddr = :upaddr order by 1"
+    d = sql(query, {'upaddr': upaddr});
+    return jsonify([dict(row) for row in d.fetchall()])
+
+@app.route('/ajax/category')
+@app.route('/ajax/category/')
+@app.route('/ajax/category/<uptour>')
+def category_list(uptour = ''):
+    query = "select tourid,tourname from tourism where uptour = :uptour order by 1"
+    d = sql(query, {'uptour': uptour});
+    return jsonify([dict(row) for row in d.fetchall()])
+
+@app.route('/ajax/random10/')
+def random_list():
+    query = "select distinct a.place_id, a.place_name, first_value(c.imgurl) over (partition by a.place_id) as imgurl \
+        from place a, (select place_id from place order by random() limit 20) b left join place_images c on b.place_id = c.place_id where a.place_id = b.place_id"
+    d = sql(query)
+    return jsonify([dict(row) for row in d.fetchall()])
+
+@app.route('/ajax/list/<locaval>/<cateval>')
+def place_list(locaval, cateval):
+    if (locaval == "all" and cateval == "all"):
+        return random_list()
+    elif locaval == "all":
+        query = "select distinct a.place_id, a.place_name, first_value(c.imgurl) over (partition by a.place_id) as imgurl \
+			from place a, (select place_id from place where cate in \
+			(with recursive t as (select * from tourism where uptour = any(:catestr) \
+			union all select a.* from tourism a, t where a.uptour = t.tourid) select tourid from t where length(tourid) = 9) order by place_name limit 50) b \
+		    left join place_images c on b.place_id = c.place_id where a.place_id = b.place_id"
+        catestr = cateval.split(",")
+        d = sql(query, {'catestr': catestr})
+    elif cateval == "all":
+        query = "select distinct a.place_id, a.place_name, first_value(c.imgurl) over (partition by a.place_id) as imgurl \
+			from place a, (select place_id from place where loca in \
+			(select addrid from addrcodes where upaddr = any(:locastr)) order by place_name limit 50) b \
+		    left join place_images c on b.place_id = c.place_id where a.place_id = b.place_id"
+        locastr = locaval.split(",")
+        d = sql(query, {'locastr': locastr})
+    else:
+        if locaval == "undefind":
+            query = "select distinct a.place_id, a.place_name, first_value(c.imgurl) over (partition by a.place_id) as imgurl \
+		    from place a, (select place_id from place where loca is null and cate in (\
+	    (with recursive t as (select * from tourism where uptour = any(:catestr) \
+            union all select a.* from tourism a, t where a.uptour = t.tourid) select tourid from t where length(tourid) = 9)) order by place_name limit 50) b \
+            left join place_images c on b.place_id = c.place_id where a.place_id = b.place_id"
+            catestr = cateval.split(",")
+            d = sql(query, {'catestr': catestr})
+        else:
+            query = "select distinct a.place_id, a.place_name, first_value(c.imgurl) over (partition by a.place_id) as imgurl \
+		    from place a, (select place_id from place where loca in \
+            (select addrid from addrcodes where upaddr = any(:locastr) and cate in \
+	    (with recursive t as (select * from tourism where uptour = any(:catestr) \
+            union all select a.* from tourism a, t where a.uptour = t.tourid) select tourid from t where length(tourid) = 9)) order by place_name limit 50) b \
+            left join place_images c on b.place_id = c.place_id where a.place_id = b.place_id"
+            locastr = locaval.split(",")
+            catestr = cateval.split(",")
+            d = sql(query, {'locastr': locastr, 'catestr': catestr})
+    return jsonify([dict(row) for row in d.fetchall()])
+
+@app.route('/ajax/namesearch/<search_str>')
+def search_list(search_str):
+    query = "select distinct a.place_id, a.place_name, first_value(c.imgurl) over (partition by a.place_id) as imgurl \
+		    from place a, (select place_id from place where place_name like :search_str order by place_name limit 50) b \
+		    left join place_images c on b.place_id = c.place_id where a.place_id = b.place_id"
+    d = sql(query, {'search_str': '%' + search_str + '%'})
+    return jsonify([dict(row) for row in d.fetchall()])
+
+@app.route('/ajax/getnames/<location>/<category>')
+def get_cateloca_names(location,category):
+    if location == "None":
+        locaname = "미지정"
+    else:
+        query = "with recursive t as (select * from addrcodes where addrid = :location union all select a.* from addrcodes a, t where a.addrid = t.upaddr ) select string_agg(addrname,' ') from (select * from t order by addrid) a"
+        res = sql(query, {'location': location})
+        locaname = res.fetchone()[0];
+
+    query = "with recursive t as (select * from tourism where tourid = :category union all select a.* from tourism a, t where a.tourid = t.uptour ) select string_agg(tourname,' > ') from (select * from t order by tourid) a"
+    res = sql(query, {'category': category})
+    catename = res.fetchone()[0];
+    return jsonify(catename=catename,locaname=locaname);
+
+@app.route('/ajax/near/<place_id>/<x>/<y>')
+def near_list(place_id, x,y):
+    query = "select distinct a.place_id, a.place_name, d.tourname, first_value(c.imgurl) over (partition by a.place_id) as imgurl, b.distance as distance \
+    from place a, ( \
+    select place_id, round((earth_distance(position, ll_to_earth(:y1,:x1))::numeric / 1000)::numeric, 2)::float as distance from place a \
+    where earth_box(ll_to_earth(:y1,:x1), 2000) @> position and a.place_id <> :place_id \
+    ) b left join place_images c on b.place_id = c.place_id, tourism d where a.place_id = b.place_id and d.tourid = substring(a.cate, 1, 3) order by 3, 5"
+    d = sql(query, {'place_id': place_id, 'x1': x, 'y1': y})
+    return jsonify([dict(row) for row in d.fetchall()])
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000)
+    app.run(host='0.0.0.0', port=8000, debug=True)
